8ea37619d625bf014402935ee8fcdcd8df2ecbab
darylnyawasha <50359940+darylnyawasha@users.noreply.github.com>
2020-09-17 13:03:09 +0200

    Map Dialogs

--- src/app/Shared/map/map.page.ts
+++ src/app/Shared/map/map.page.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild, ElementRef, Input, NgZone } from '@angular/core';
+//import { MapsAPILoader, AgmMap } from '@agm/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+// import { AgmCoreModule } from '@agm/core/lib/core.module';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-map',
@@ -10,22 +15,131 @@ import { Router } from '@angular/router';
 
 export class MapPage implements OnInit {
 
-  constructor(private route: Router) { }
+  title: string = 'AGM project';
+  latitude: number;
+  longitude: number;
+  zoom: number;
+  address: string;
+  private geoCoder;
+
+  @ViewChild('search',{static:false})
+  public searchElementRef: ElementRef;
+
+  constructor(private route: Router, public dialog: MatDialog, private alert: AlertController,
+    private positionalert: AlertController, private ngZone: NgZone) { }
+    //private mapsAPILoader: MapsAPILoader
 
   ngOnInit() {
-    this.getLocation();
+    console.log("map log");
+    this.presentAlert();
+  }
+  //   this.mapsAPILoader.load().then(() => {
+  //     this.setCurrentLocation();
+  //     this.geoCoder = new google.maps.Geocoder;
+
+  //     let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+  //       types: ["address"]
+  //     });
+  //     autocomplete.addListener("place_changed", () => {
+  //       this.ngZone.run(() => {
+  //         //get the place result
+  //         let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+  //         //verify result
+  //         if (place.geometry === undefined || place.geometry === null) {
+  //           return;
+  //         }
+
+  //         //set latitude, longitude and zoom
+  //         this.latitude = place.geometry.location.lat();
+  //         this.longitude = place.geometry.location.lng();
+  //         this.zoom = 12;
+  //       });
+  //     });
+  //   });
+  // }
+
+  // // Get Current Location Coordinates
+  // private setCurrentLocation() {
+  //   if ('geolocation' in navigator) {
+  //     navigator.geolocation.getCurrentPosition((position) => {
+  //       this.latitude = position.coords.latitude;
+  //       this.longitude = position.coords.longitude;
+  //       this.zoom = 8;
+  //       this.getAddress(this.latitude, this.longitude);
+  //     });
+  //   }
+  // }
+
+
+  // markerDragEnd($event: any) {
+  //   console.log($event);
+  //   this.latitude = $event.coords.lat;
+  //   this.longitude = $event.coords.lng;
+  //   this.getAddress(this.latitude, this.longitude);
+  // }
+
+  // getAddress(latitude, longitude) {
+  //   this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+  //     console.log(results);
+  //     console.log(status);
+  //     if (status === 'OK') {
+  //       if (results[0]) {
+  //         this.zoom = 12;
+  //         this.address = results[0].formatted_address;
+  //       } else {
+  //         window.alert('No results found');
+  //       }
+  //     } else {
+  //       window.alert('Geocoder failed due to: ' + status);
+  //     }
+
+  //   });
+  // }
+
+
+  async presentAlert() {
+    const alert = await this.alert.create({
+      cssClass: 'Confirmations',
+      header: 'Allow app to use your location:',
+      buttons: [
+        {text: 'When using the app',
+        handler: () => {
+          this.getLocation();
+        }},
+        {text: 'Always',
+        handler: () => {
+          this.getLocation();
+        }},
+        {text: 'Never'}
+    ]
+    });
+    await alert.present();
   }
 
-  getLocation(): void {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(this.showPosition);
-  } else {
-    console.log("Geolocation is not supported by this browser.")
-  }
+  async getLocation() {
+    console.log("getLocation()")
+    
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.showPosition);
+    } else {
+      console.log("Geolocation is not supported by this browser.")
+    }
 }
 
-  showPosition(position): void {
-    alert("Current Location -> Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
+async showPosition(position) {
+    console.log("showPosition()")
+    console.log("Position: " + position + " Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude)
+
+    const alert = await this.positionalert.create({
+      cssClass: 'Confirmations',
+      header: 'Your loaction: Latitude:',
+      buttons: [
+        {text: 'OK'}
+    ]
+    });    
+    await alert.present();
+    //alert("Current Location -> Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
     console.log("Position: " + position + " Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude)
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,26 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
-
+import { MatDialogModule, } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { AgmCoreModule } from '@agm/core/lib/core.module';
+//import { MapsAPIWrapper, AgmMap } from '@agm/core';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, 
-    IonicModule.forRoot(), 
+    IonicModule.forRoot(),
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
-    AngularFireStorageModule
+    AngularFireStorageModule,
+    MatDialogModule,
+    BrowserAnimationsModule
+    // AgmCoreModule.forRoot({
+    //   apiKey: 'AIzaSyCiYPfg_zOdMhxyyLSkUvi121pzdH6KAbU',
+    //   libraries: ['places']
+    // })
   ],
   providers: [
     StatusBar,
@@ -30,7 +39,8 @@ import { environment } from 'src/environments/environment';
 
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
 
-    AngularFirestore,
+    AngularFirestore
+    //,MapsAPIWrapper
   ],
   bootstrap: [AppComponent]
 })
